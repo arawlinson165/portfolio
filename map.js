fa9d93a118caacd4c73f145a93abaefa6dbb634c
arawlinson165 <arawlinson165@gmail.com>
2026-06-26 14:15:03 -0500

    delete pages, fix links

--- map.js
+++ map.js
@@ -1,5 +1,6 @@
 let scroll = document.querySelector(".scroll-container");
 let video = document.querySelector(".video");
+// let allButtons = document.querySelector(".video-navbtn");
 let aboutButton = document.querySelector("#aboutButton");
 let projectsButton = document.querySelector("#projectsButton");
 let contactButton = document.querySelector("#contactButton");
@@ -9,22 +10,34 @@ let contactButton = document.querySelector("#contactButton");
 
 let scrollFraction = 0;
 
-window.addEventListener("scroll", function(){
-    if(video.duration){
-        scrollFraction = -1.25 * (scroll.getBoundingClientRect().top / scroll.clientHeight);
+window.addEventListener("scroll", function () {
+    if (video.duration) {
+        scrollFraction = -1.5 * (scroll.getBoundingClientRect().top / scroll.clientHeight);
         video.currentTime = video.duration * scrollFraction;
-        aboutButton.style.left= (scrollFraction * 100 - 10) + "%";
-        projectsButton.style.left= (scrollFraction * 100 - 50) + "%";
-        contactButton.style.left= (scrollFraction * 100 - 90) + "%";
+        aboutButton.style.left = (scrollFraction * 100 - 25) + "%";
+        projectsButton.style.left = (scrollFraction * 100 - 65) + "%";
+        contactButton.style.left = (scrollFraction * 100 - 105) + "%";
+
+        if (scrollFraction > 1.34) {
+            aboutButton.style.opacity = 0;
+            projectsButton.style.opacity = 0;
+            contactButton.style.opacity = 0;
+        } else if(scrollFraction > 1.26){
+            aboutButton.style.opacity = (1 / (scrollFraction * 3));
+            projectsButton.style.opacity = (1 / (scrollFraction * 3));
+            contactButton.style.opacity = (1 / (scrollFraction * 3));
+        } else{
+            aboutButton.style.opacity = 1;
+            projectsButton.style.opacity = 1;
+            contactButton.style.opacity = 1;
+        }
+
         console.log(scrollFraction);
     }
 });
 
-// aboutButton.addEventListener("click", function(){
-//     event.preventDefault();
-//     window.location.href = "about-me.html";
-// });
+
 
 video.addEventListener('loadedmetadata', () => {
-  video.currentTime = 0; 
+    video.currentTime = 0;
 });
